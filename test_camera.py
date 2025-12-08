a9ac5485dd77c07e3bc078dd6d2b1f0ab922f4c0
Joo Heejeong <joohee040704@gmail.com>
2025-12-09 08:35:15 +0900

    Update test_camera.py

--- test_camera.py
+++ test_camera.py
@@ -1,61 +1,13 @@
-from collections import deque
+import pygame
 import numpy as np
-import cv2
 
-from inference.extract_landmarks import extract_landmarks
-from inference.preprocessor import process_to_feature
-from inference.TFLite import AppInferenceTFLite
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+clock = pygame.time.Clock()
 
-SEQ_LEN = 30
-buffer = deque(maxlen=SEQ_LEN)
-
-def run_realtime_inference():
-
-    infer = AppInferenceTFLite()
-
-    # ======== V4L2 장치 사용 =========
-    cam_device = "/dev/video10"
-    print(f"📸 opening camera: {cam_device}")
-    cap = cv2.VideoCapture(cam_device)
-
-    if not cap.isOpened():
-        print("❌ camera open failed")
-        return
-
-    print("✅ camera opened")
-
-    frame_count = 0
-
-    while True:
-        ret, frame = cap.read()
-        print("ret:", ret)
-
-        if not ret:
-            continue
-
-        frame_count += 1
-        if frame_count % 10 == 0:
-            print(f"frames read: {frame_count}")
-
-        # ======== Landmark 추출 ========
-        landmarks = extract_landmarks(frame)
-        if landmarks is None:
-            continue
-
-        # ======== Feature 변환 ========
-        feature = process_to_feature(landmarks)
-        buffer.append(feature)
-
-        # ======== 추론 실행 ========
-        if len(buffer) == SEQ_LEN:
-            seq_array = np.array(buffer)
-            pred_word, pred_prob = infer.predict_from_array(seq_array)
-            print(f"[PRED] {pred_word}: {pred_prob.max():.3f}")
-
-        # ======== 화면 표시 ========
-        cv2.imshow("frame", frame)
-        if cv2.waitKey(1) == ord("q"):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+# frame: np.ndarray(BGR)
+frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+frame_surface = pygame.surfarray.make_surface(np.rot90(frame))
+screen.blit(frame_surface, (0, 0))
+pygame.display.flip()
+clock.tick(30)
