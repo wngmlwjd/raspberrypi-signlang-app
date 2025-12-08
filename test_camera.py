9e96c22c954ad3148ea93299b4dc0391cfe3472b
Joo Heejeong <joohee040704@gmail.com>
2025-12-09 08:38:55 +0900

    Update test_camera.py

--- test_camera.py
+++ test_camera.py
@@ -1,39 +1,30 @@
 import pygame
-import cv2
 import numpy as np
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import cv2
 
 # 초기화
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
 clock = pygame.time.Clock()
 
-# 카메라 열기
-cap = cv2.VideoCapture(0)  # 또는 /dev/video10 등 카메라 장치 경로
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 30
+rawCapture = PiRGBArray(camera, size=(640, 480))
 
-if not cap.isOpened():
-    print("카메라 열기 실패")
-    exit()
-
-running = True
-while running:
-    ret, frame = cap.read()
-    if not ret:
-        continue
-
-    # OpenCV BGR → RGB
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-    # numpy → pygame surface
-    frame_surface = pygame.surfarray.make_surface(np.rot90(frame_rgb))
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    image = frame.array
+    frame_surface = pygame.surfarray.make_surface(np.rot90(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)))
     screen.blit(frame_surface, (0, 0))
     pygame.display.flip()
-
-    # 이벤트 처리 (종료)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
     clock.tick(30)
 
-cap.release()
-pygame.quit()
+    rawCapture.truncate(0)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            camera.close()
+            pygame.quit()
+            exit()
