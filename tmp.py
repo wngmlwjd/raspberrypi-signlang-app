6a5e0a9a3cea445f0a715aecbda60c03bfa09ef7
Joo Heejeong <joohee040704@gmail.com>
2025-12-09 18:17:44 +0900

    Create tmp.py

---
+++ tmp.py
@@ -0,0 +1,17 @@
+import subprocess
+import time
+
+counter = 0
+
+while True:
+    # 파일 이름 생성 (3자리 0패딩)
+    filename = f"test_{counter:03d}.jpg"
+    
+    # rpicam-dpeg 명령어 실행
+    subprocess.run(["rpicam-dpeg", "--output", filename, "--timeout", "500"])
+    
+    # 카운터 증가
+    counter += 1
+    
+    # 반복 간격 (timeout이 500ms이므로 sleep은 선택 사항)
+    time.sleep(0.1)
