446eaaa1e6c05b5c7a0d077432e07038010c8ab6
Joo Heejeong <joohee040704@gmail.com>
2025-12-09 18:22:00 +0900

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -5,7 +5,7 @@ from config.config import FRAMES_DIR
 
 counter = 0
 
-while counter < 1000:
+while counter < 10:
     filename = f"test_{counter:03d}.jpg"
     # timeout을 1ms로 설정 → 사실상 바로 촬영 후 저장
     subprocess.run(["rpicam-jpeg", "--output", os.path.join(FRAMES_DIR, filename), "--timeout", "1"])
