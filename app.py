88990eeeebd9c31fc1c607f124c56a7b046c380f
Joo Heejeong <joohee040704@gmail.com>
2025-12-09 16:27:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,29 @@
-from flask import Flask, Response, render_template
-from camera.camera_stream import CameraStream
+# app.py
+from flask import Flask, Response
+from camera_stream import CameraStream
+from config.config import CMD  # rpicam-vid 명령어
 
 app = Flask(__name__)
 
-# 카메라 명령어
-CAM_CMD = [
-    "rpicam-vid",
-    "-t", "0",
-    "-o", "-",
-    "--width", "640",
-    "--height", "480",
-    "--framerate", "30"
-]
+camera = CameraStream(cmd=CMD)
 
-camera = CameraStream(cmd=CAM_CMD)
-
-def gen_frames():
+def generate_frames():
     while True:
-        frame = camera.get_frame_bytes()
-        if frame is None:
+        frame_bytes = camera.get_frame_bytes()
+        if frame_bytes is None:
             continue
+        # MJPEG 스트림 형식으로 변환
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
 @app.route("/video_feed")
 def video_feed():
-    return Response(gen_frames(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames(),
+                    mimetype="multipart/x-mixed-replace; boundary=frame")
 
 @app.route("/")
 def index():
-    return render_template("index.html")  # <img src="/video_feed">
+    return "<h1>Raspberry Pi Camera Stream</h1><img src='/video_feed'>"
 
 if __name__ == "__main__":
     try:
