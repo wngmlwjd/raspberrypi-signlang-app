ed392a4a5d5174fac87dd564135b16437cd33774
Joo Heejeong <joohee040704@gmail.com>
2025-12-09 18:27:50 +0900

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,12 +1,18 @@
 import subprocess
 import os
+import time
 
-from config.config import FRAMES_DIR
+from config.config import RAW_DIR
 
 counter = 0
 
-while counter < 10:
-    filename = f"test_{counter:03d}.jpg"
-    # timeout을 1ms로 설정 → 사실상 바로 촬영 후 저장
-    subprocess.run(["rpicam-jpeg", "--output", os.path.join(FRAMES_DIR, filename), "--timeout", "1"])
+while True:
+    # 파일 이름 생성 (test_000.mp4, test_001.mp4, ...)
+    video_filename = os.path.join(RAW_DIR, f"test_{counter:03d}.mp4")
+    
+    # 5초 동안 비디오 녹화
+    subprocess.run(["rpicam-vid", "-t", "5000", "-o", video_filename])
+    
     counter += 1
+    # 녹화가 끝나면 바로 다음 영상 녹화
+    # 필요하면 sleep으로 간격 조절 가능
